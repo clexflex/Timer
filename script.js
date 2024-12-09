611ecd2f4687048250a1a510ea72c203e3dbd7d5
Yashraj Ghosalkar <74045394+clexflex@users.noreply.github.com>
2024-12-09 19:04:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-const countDownDate = new Date("November 29, 2024 17:00:00").getTime();
+const countDownDate = new Date("March 2, 2025 17:00:00").getTime();
 
 // Update the count down every 10 milliseconds
 const x = setInterval(function() {
